40e54250fcffbb96b42678fec012f1a338f64368
navyFox <lina.666@list.ru>
2021-12-21 15:31:56 +0300

    сделала график с помощью плагина

--- src/components/dashboard-components/chart-tokens.vue
+++ src/components/dashboard-components/chart-tokens.vue
@@ -47,7 +47,26 @@
       </div>
     </div>
     <div class="chart-body">
-      <GChart type="ColumnChart" :data="chartData" :options="chartOptions" />
+      <svg
+        style="width: 0; height: 0; position: absolute"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <linearGradient id="my-gradient" x2="1" y2="1">
+          <stop offset="0%" stop-color="#447799" />
+          <stop offset="50%" stop-color="#224488" />
+          <stop offset="100%" stop-color="#112266" />
+        </linearGradient>
+      </svg>
+      <GChart
+        type="ColumnChart"
+        :data="chartData"
+        :options="chartOptions"
+        @ready="chr"
+        @select="chr"
+        @onmouseover="chr"
+        @onmouseout="chr"
+      />
     </div>
   </DashboardBlock>
 </template>
@@ -55,32 +74,118 @@
 <script>
 import DashboardBlock from "./dashboard-block";
 import { GChart } from "vue-google-charts";
+import { CHARTS_TEST_DATA } from "../../data/CHARTS_TEST_DATA";
+
+//TODO графики полная хрень. Нужно переделывать
 
 export default {
   name: "chart-tokens",
   // eslint-disable-next-line vue/no-unused-components
   components: { DashboardBlock, GChart },
+  computed: {
+    chartDatat() {
+      const join = this.isJoined;
+      const chartData = [];
+      chartData.push(Object.keys(CHARTS_TEST_DATA[0]));
+      if (join.day) {
+        CHARTS_TEST_DATA.forEach((i) => {
+          const dateNow =
+            new Date().getDate() + "." + (Number(new Date().getMonth()) + 1);
+          if (i.date === String(dateNow)) {
+            chartData.push(Object.values(i));
+          }
+        });
+        // eslint-disable-next-line no-empty
+      }
+      return chartData;
+    },
+  },
+  mounted() {
+    console.log(
+      new Date().getDate() + "." + (Number(new Date().getMonth()) + 1)
+    );
+  },
   data() {
     return {
       isJoined: {
         year: false,
-        month: false,
-        day: true,
+        month: true,
+        day: false,
       },
       chartData: [
-        ["Year", "Sales", "Expenses", "Profit"],
-        ["2014", 1000, 400, 200],
-        ["2015", 1170, 460, 250],
-        ["2016", 660, 1120, 300],
-        ["2017", 1030, 540, 350],
+        ["date", "mainFoil", "bonusFoil"],
+        ["1.09", 1.7, 0],
+        ["2.09", 1, 0],
+        ["3.09", 1.7, 0],
+        ["4.09", 1.8, 0],
+        ["5.09", 2.5, 0.5],
+        ["6.09", 2.1, 0],
+        ["7.09", 1.6, 0],
+        ["8.09", 2, 0],
+        ["9.09", 1.6, 0],
+        ["10.09", 2.4, 0],
+        ["11.09", 1.6, 0],
+        ["12.09", 2.9, 0],
       ],
       chartOptions: {
         chart: {
           title: "Company Performance",
           subtitle: "Sales, Expenses, and Profit: 2014-2017",
         },
+        backgroundColor: "none",
+        colors: ["#21D4FD", "#E40ECF"],
+        explorer: { axis: "horizontal" },
+        isStacked: true,
+        enableInteractivity: false,
+        bar: {
+          groupWidth: 20,
+        },
+        vAxis: {
+          minorGridlines: {
+            color: "none",
+            count: 0,
+          },
+          gridlines: {
+            color: "#252A36",
+            count: 3,
+          },
+          textStyle: {
+            color: "#FFFFFF",
+            fontName: "LabGrotesque",
+            fontSize: 12,
+            bold: true,
+          },
+        },
+        hAxis: {
+          gridlines: {
+            units: {
+              days: { format: "DD/MM" },
+            },
+          },
+          slantedText: true,
+          textStyle: {
+            color: "#FFFFFF",
+            fontName: "LabGrotesque",
+            fontSize: 10,
+            bold: true,
+          },
+        },
+        chartArea: { left: 30, top: 24, width: "90%", height: "75%" },
+        legend: { position: "none" },
+        tooltip: { trigger: "none" },
       },
     };
+  },
+  methods: {
+    chr(ctx) {
+      let chartColumns = ctx.container.getElementsByTagName("rect");
+      setTimeout(() => {
+        Array.prototype.forEach.call(chartColumns, (column) => {
+          column.setAttribute("rx", 3);
+          column.setAttribute("ry", 3);
+        });
+      }, 0);
+    },
   },
 };
 </script>
@@ -92,7 +197,7 @@ export default {
     align-items: baseline;
     justify-content: start;
     gap: 10px;
-    padding: 25px 24px;
+    padding: 16px 24px 20px;
 
     &__img {
       width: 37px;
@@ -109,7 +214,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 24px 24px;
+  padding: 0 24px;
 
   &__title {
     font-family: LabGrotesque, serif;
---
+++ src/data/CHARTS_TEST_DATA.js
@@ -0,0 +1,102 @@
+export const CHARTS_TEST_DATA = [
+  {
+    date: "1.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "2.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "3.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "4.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "5.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "6.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "6.08",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "2.09",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "10.09",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "12.09",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "2.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "4.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "5.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "15.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "16.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "17.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "18.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "19.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "20.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+  {
+    date: "21.12",
+    mainFoil: 1.7,
+    bonusFoil: 0.2,
+  },
+];
